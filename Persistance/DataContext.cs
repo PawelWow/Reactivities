80c66ac452de1f524ba2005b63140061d5ed0343
pawel <pawel@webperfect.pl>
2020-08-31 20:41:16 +0200

    value controller, data context and sqlite

---
+++ Persistance/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Domain;
+
+namespace Persistance
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Value> Values { get; set; }
+
+        public DataContext(DbContextOptions options) : base( options)
+        {
+
+        }
+    }
+}
