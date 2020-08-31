33c23e069da9290751b3b07cac2d3f2e8afb73ed
pawel <pawel@webperfect.pl>
2020-08-31 21:21:19 +0200

    First data in database

--- Persistance/DataContext.cs
+++ Persistance/DataContext.cs
@@ -12,5 +12,14 @@ namespace Persistance
         {
 
         }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Value>().HasData(
+                    new Value { Id = 1, Name = "Value 1-1" },
+                    new Value { Id = 2, Name = "Value 1-2" },
+                    new Value { Id = 3, Name = "Value 1-3" }
+                );
+        }
     }
 }
