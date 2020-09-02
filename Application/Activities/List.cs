998c112cd2d9349b00533c8a6eaad880e2f7b56a
pawel <pawel@webperfect.pl>
2020-09-02 22:01:52 +0200

    First query handler

---
+++ Application/Activities/List.cs
@@ -0,0 +1,32 @@
+﻿using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistance;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Activities
+{
+    public class List
+    {
+        public class Query : IRequest<List<Activity>> { }
+
+        public class Handler : IRequestHandler<Query, List<Activity>>
+        {
+            private readonly DataContext m_context;
+
+            public Handler(DataContext context)
+            {
+                m_context = context;
+            }
+
+            public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var activities = await m_context.Activities.ToListAsync();
+                return activities;
+            }
+        }
+    }
+}
