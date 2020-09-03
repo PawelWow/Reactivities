a27758ea771d8f6b8b7581764a40865a7933e614
pawel <pawel@webperfect.pl>
2020-09-03 18:04:33 +0200

    client-app: activity list

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Grid, List } from 'semantic-ui-react';
 import { IActivity } from '../../../app/models/activity';
 
+import ActivityList from './ActivityList';
+
 interface IProps {
     activities: IActivity[]
 }
@@ -10,9 +12,7 @@ const ActivityDashboard: React.FC<IProps> = ({ activities }) => {
     return (
         <Grid>
             <Grid.Column width={10}>
-                <List>
-                    {activities.map(activity => (<List.Item key={activity.id}>{activity.title}</List.Item>))}
-                </List>
+                <ActivityList activities={activities} />
             </Grid.Column>
         </Grid>
     );
