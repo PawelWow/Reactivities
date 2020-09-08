c6acba243296d01d022126428f04d1ea0fbe8031
pawel <pawel@webperfect.pl>
2020-09-08 16:16:40 +0200

    client-app: setup date widgets

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router } from 'react-router-dom';
+import dateFnsLocalizer from 'react-widgets-date-fns';
 import { createBrowserHistory } from 'history';
 import 'react-toastify/dist/ReactToastify.min.css';
+import 'react-widgets/dist/css/react-widgets.css';
 import './app/layout/styles.css';
 import App from './app/layout/App';
 import * as serviceWorker from './serviceWorker';
 import ScrollToTop from './app/layout/ScrollTop';
+
+dateFnsLocalizer();
 
 export const history = createBrowserHistory();
 
