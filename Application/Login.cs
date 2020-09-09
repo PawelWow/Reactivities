2bc8a7d62dcd13b14b65dc0e5d355d19d410cda3
pawel <pawel@webperfect.pl>
2020-09-09 14:20:18 +0200

    API: login handler

---
+++ Application/Login.cs
@@ -0,0 +1,64 @@
+﻿using Application.Errors;
+using Domain;
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Persistance;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public class Login
+    { 
+        public class Query : IRequest<AppUser> 
+        {
+            public string Email { get; set; }
+            public string Password { get; set; }
+        }
+
+        public class QueryValidator : AbstractValidator<Query>
+        {
+            public QueryValidator()
+            {
+                RuleFor(x => x.Email).NotEmpty();
+                RuleFor(x => x.Password).NotEmpty();
+            }
+        }
+
+        public class Handler : IRequestHandler<Query, AppUser>
+        {
+            private readonly UserManager<AppUser> m_userManager;
+            private readonly SignInManager<AppUser> m_signInManager;
+
+            public Handler(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+            {
+                m_userManager = userManager;
+                m_signInManager = signInManager;
+            }
+
+            public async Task<AppUser> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var user = await m_userManager.FindByEmailAsync(request.Email);
+                if (user == null)
+                {
+                    throw new RestException(HttpStatusCode.Unauthorized);
+                }
+
+                var result = await m_signInManager.CheckPasswordSignInAsync(user, request.Password, false);
+                if (result.Succeeded)
+                {
+                    // TODO: generate token
+                    return user;
+                }
+
+                throw new RestException(HttpStatusCode.Unauthorized);
+            }
+        }
+    
+    }
+}
