fa3c59a18c009b4a158be31267eaf95b78eedfd9
pawel <pawel@webperfect.pl>
2020-09-10 18:56:56 +0200

    API: added custom policy (edit and delete activity allowed only for hosts)

---
+++ Infrastructure/Security/IsHostRequirement.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Persistance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Security
+{
+    public class IsHostRequirement : IAuthorizationRequirement
+    {
+
+    }
+
+    public class IsHostRequirementHandler : AuthorizationHandler<IsHostRequirement>
+    {
+        private readonly IHttpContextAccessor m_httpContextAccessor;
+        private readonly DataContext m_context;
+
+        public IsHostRequirementHandler(IHttpContextAccessor httpContextAccessor, DataContext context)
+        {
+            m_httpContextAccessor = httpContextAccessor;
+            m_context = context;
+        }
+
+
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, IsHostRequirement requirement)
+        {
+            var currentUserName = m_httpContextAccessor.HttpContext.User?.Claims
+                .SingleOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value;
+
+            var activityId = Guid.Parse(m_httpContextAccessor.HttpContext.Request.RouteValues
+                .SingleOrDefault(x => x.Key == "id").Value.ToString());
+
+            var activity = m_context.Activities.FindAsync(activityId).Result;
+
+            var host = activity.UserActivities.FirstOrDefault(x => x.IsHost);
+            if(host?.AppUser?.UserName == currentUserName)
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
