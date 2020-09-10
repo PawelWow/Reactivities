db75bbec9277c494736cd5f853b874d1a7a58975
pawel <pawel@webperfect.pl>
2020-09-10 18:34:57 +0200

    API: unattend activity feature

---
+++ Application/Activities/Unattend.cs
@@ -0,0 +1,69 @@
+﻿using Application.Errors;
+using Application.interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistance;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Activities
+{
+    public class Unattend
+    {
+        public class Command : IRequest
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext m_context;
+            private readonly IUserAccessor m_userAccessor;
+            public Handler(DataContext context, IUserAccessor userAccessor)
+            {
+                m_context = context;
+                m_userAccessor = userAccessor;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+
+                var activity = await m_context.Activities.FindAsync(request.Id);
+
+                if (activity == null)
+                {
+                    throw new RestException(HttpStatusCode.NotFound, new { Activity = "Could not find activity" });
+                }
+
+                var user = await m_context.Users.SingleOrDefaultAsync(x => x.UserName == m_userAccessor.GetCurrentUsername());
+
+                var attendance = await m_context.UserActivities.SingleOrDefaultAsync(x => x.ActivityId == activity.Id && x.AppUserId == user.Id);
+
+                if(attendance == null)
+                {
+                    return Unit.Value;
+                }
+
+                if (attendance.IsHost)
+                {
+                    throw new RestException(HttpStatusCode.BadRequest, new { Attendance = "You cannot remove yourself as host!" });
+                }
+
+                m_context.UserActivities.Remove(attendance);
+
+                var isSuccess = await m_context.SaveChangesAsync() > 0;
+
+                if(isSuccess)
+                {
+                    return Unit.Value;
+                }
+
+                throw new Exception("Problem saving changes!");
+            }
+        }
+    }
+}
