6f6893bbc73e7c406b93391a0aa072abd5c5c9f2
pawel <pawel@webperfect.pl>
2020-09-16 20:06:01 +0200

    API: basic configuration for Cloudinary

--- API/Startup.cs
+++ API/Startup.cs
@@ -4,6 +4,7 @@ using Application.interfaces;
 using AutoMapper;
 using Domain;
 using FluentValidation.AspNetCore;
+using Infrastructure.Photos;
 using Infrastructure.Security;
 using MediatR;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -101,6 +102,7 @@ namespace API
 
             services.AddScoped<IJwtGenerator, JwtGenerator>();
             services.AddScoped<IUserAccessor, UserAccessor>();
+            services.Configure<CloudinarySettings>(Configuration.GetSection("Cloudinary"));
             
         }
 
