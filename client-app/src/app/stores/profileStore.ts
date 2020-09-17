89664a777a8ed1249ed5188561bc2b3d0b6bb10a
pawel <pawel@webperfect.pl>
2020-09-17 15:26:23 +0200

    client-app: correct IProfile and buttons in photo profiles pane

--- client-app/src/app/stores/profileStore.ts
+++ client-app/src/app/stores/profileStore.ts
@@ -1,5 +1,5 @@
 import { RootStore } from './rootStore';
-import { action, observable, runInAction } from 'mobx';
+import { action, observable, runInAction, computed } from 'mobx';
 import { IProfile } from '../models/profile';
 import agent from '../api/agent';
 
@@ -14,12 +14,21 @@ export default class ProfileStore {
     // Powinno być false. Ustawiliśmy na true, bo dostajemy po drodze nulla, a to jest najprostszy sposób na jego rozwiązanie
     @observable loadingProfile = true;
 
+    @computed get isCurrentUser() {
+        if(this.rootStore.userStore.user && this.profile) {
+            return this.rootStore.userStore.user.username === this.profile.username;
+        } else {
+            return false;
+        }
+    }
+
     @action loadProfile = async(username: string) => {
         this.loadingProfile = true;
         try {
             const profile = await agent.Profiles.get(username);
+
             runInAction(() => {
-                this.profile = profile;                
+                this.profile = profile;            
             });
         } catch (error) {
             console.log(error);
