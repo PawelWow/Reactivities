0c6cf348f9f647aa913b36dfa5031899129f6b4d
pawel <pawel@webperfect.pl>
2020-09-17 15:51:18 +0200

    client-app: widget dropzone for drag and drop images - basic configuration

--- client-app/src/app/common/photoUpload/PhotoUploadWidget.tsx
+++ client-app/src/app/common/photoUpload/PhotoUploadWidget.tsx
@@ -1,13 +1,17 @@
 import { observer } from 'mobx-react-lite';
-import React, { Fragment } from 'react';
-import { Grid, Header } from 'semantic-ui-react';
+import React, { Fragment, useState } from 'react';
+import { Grid, Header, Image } from 'semantic-ui-react';
+
+import PhotoWidgetDropzone from './PhotoWidgetDropzone';
 
 const PhotoUploadWidget = () => {
+    const [files, setFiles] = useState<any[]>([]);
+
     return (
         <Fragment>
             <Grid>
                 <Grid.Column width={4}>
-                    <Header color='teal' sub content='Step 1 - Add Photo' />
+                    <PhotoWidgetDropzone setFiles={setFiles} />
                 </Grid.Column>
                 <Grid.Column width={1} />
                 <Grid.Column width={4}>
@@ -16,6 +20,10 @@ const PhotoUploadWidget = () => {
                 <Grid.Column width={1} />
                 <Grid.Column width={4}>
                     <Header color='teal' sub content='Step 3 - Preview & Upload' />
+                    {
+                        files.length > 0 && <Image src={files[0].preview} />
+                    }
+                    
                 </Grid.Column>
             </Grid>
         </Fragment>
