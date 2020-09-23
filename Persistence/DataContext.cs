1799f7e0bb291c7d5802fbb60d5c6e1108c275f8
pawel <pawel@webperfect.pl>
2020-09-23 15:00:36 +0200

    API: correct for entity for followers relationship

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -48,7 +48,7 @@ namespace Persistence
                     .OnDelete(DeleteBehavior.Restrict);
 
                 builder.HasOne(o => o.Target)
-                    .WithMany(f => f.Followings)
+                    .WithMany(f => f.Followers)
                     .HasForeignKey(o => o.TargetId)
                     .OnDelete(DeleteBehavior.Restrict);
             });
