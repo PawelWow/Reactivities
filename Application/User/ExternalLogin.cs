1f3a122b685d43709b95516e25f9f2622159f996
pawel <pawel@webperfect.pl>
2020-09-24 21:13:43 +0200

    API: handling facebook login

---
+++ Application/User/ExternalLogin.cs
@@ -0,0 +1,97 @@
+﻿using Application.Errors;
+using Application.interfaces;
+using Domain;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.User
+{
+    public class ExternalLogin
+    {
+        public class Query : IRequest<User>
+        {
+            public string AccessToken { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, User>
+        {
+            private readonly UserManager<AppUser> m_userManager;
+            private readonly IFacebookAccessor m_facebookAccessor;
+            private readonly IJwtGenerator m_jwtGenerator;
+
+            public Handler(UserManager<AppUser> userManager, IFacebookAccessor facebookAccessor, IJwtGenerator jwtGenerator)
+            {
+                m_userManager = userManager;
+                m_facebookAccessor = facebookAccessor;
+                m_jwtGenerator = jwtGenerator;
+            }
+
+            public async Task<User> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var userInfo = await m_facebookAccessor.FacebookLogin(request.AccessToken);
+                if (userInfo == null)
+                {
+                    throw new RestException(HttpStatusCode.BadRequest, new { User = "Problem validating token" });
+                }
+
+                var user = await m_userManager.FindByEmailAsync(userInfo.Email);
+                if (user == null)
+                {
+                    user = await this.CreateUser(userInfo);
+                }
+
+                return new User
+                {
+                    DisplayName = user.DisplayName,
+                    Token = m_jwtGenerator.CreateToken(user),
+                    Username = user.UserName,
+                    Image = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
+                };
+            }
+
+            /// <summary>
+            /// Creates new user in database for facebook login
+            /// </summary>
+            /// <param name="userInfo">User descriptor</param>
+            /// <returns>User</returns>
+            private async Task<AppUser> CreateUser(FacebookUserInfo userInfo)
+            {
+                AppUser user = new AppUser
+                {
+                    DisplayName = userInfo.Name,
+                    Id = userInfo.Id,
+                    Email = userInfo.Email,
+                    UserName = $"fb_{userInfo.Id}"
+                };
+
+                var photo = new Photo
+                {
+                    // to distinguish from cloudinary photo
+                    Id = $"fb_{userInfo.Id}",
+                    Url = userInfo.Picture.Data.Url,
+                    IsMain = true
+                };
+
+                user.Photos.Add(photo);
+
+                var result = await m_userManager.CreateAsync(user);
+                if (!result.Succeeded)
+                {
+                    throw new RestException(HttpStatusCode.BadRequest, new { User = "Problem creating user" });
+                }
+
+                return user;
+
+            }
+        }
+
+    }
+}
---
+++ Infrastructure/Security/FacebookAccessor.cs
@@ -0,0 +1,69 @@
+﻿using Application.interfaces;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Security
+{
+    public class FacebookAccessor : IFacebookAccessor
+    {
+        private readonly HttpClient m_httpClient;
+        private readonly IOptions<FacebookAppSettings> m_config;
+
+        public FacebookAccessor(IOptions<FacebookAppSettings> config)
+        {
+            m_config = config;
+            m_httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("https://graph.facebook.com")
+            };
+
+            m_httpClient.DefaultRequestHeaders
+                .Accept
+                .Add(new MediaTypeWithQualityHeaderValue("application/json"));        
+        }
+
+        public async Task<FacebookUserInfo> FacebookLogin(string accessToken)
+        {
+            // is token valid?
+            var verifyToken = await m_httpClient
+                .GetAsync($"debug_token?input_token={accessToken}&access_token={m_config.Value.AppId}|{m_config.Value.AppSecret}");
+
+            if (!verifyToken.IsSuccessStatusCode)
+            {
+                return await Task.FromResult<FacebookUserInfo>(null);
+            }
+
+            var result = await this.GetAsync<FacebookUserInfo>(
+                            accessToken,
+                            "me",
+                            "fields=name,email,picture.width(100).height(100)");
+
+            return result;
+        }
+
+        /// <summary>
+        /// Provides facebook user info
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="accessToken">Token</param>
+        /// <param name="endpoint">Endpoint (e.g. "me")</param>
+        /// <param name="args">What do we need (eg.fields=name,email,picture.width(100).height(100) ) </param>
+        /// <returns>user info in type</returns>
+        private async Task<T> GetAsync<T>(string accessToken, string endpoint, string args)
+        {
+            var response = await m_httpClient.GetAsync($"{endpoint}?access_token={accessToken}&{args}");
+            if (!response.IsSuccessStatusCode)
+            {
+                return await Task.FromResult<T>(default);
+            }
+
+            var result = await response.Content.ReadAsStringAsync();
+
+            return JsonConvert.DeserializeObject<T>(result);
+        }
+    }
+}
