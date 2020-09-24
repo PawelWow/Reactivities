5dc0d7d59b99add588a7235214120638cf355dc0
pawel <pawel@webperfect.pl>
2020-09-24 10:57:58 +0200

    client-app: safari date bug resolved

--- client-app/src/app/common/util/util.ts
+++ client-app/src/app/common/util/util.ts
@@ -2,14 +2,16 @@ import { IUser } from "../../models/user";
 import { IActivity, IAttendee } from "../../models/activity";
 
 export const combineDateAndTime = (date: Date, time: Date) => {
-    const timeString = time.getHours() + ':' + time.getMinutes() + ':00';
 
-    const year = date.getFullYear();
-    
-    const month = date.getMonth() + 1; // because it starts at 0
-    const day = date.getDate();
+    // this does not work on safari
+    // const timeString = time.getHours() + ':' + time.getMinutes() + ':00';
+    // const year = date.getFullYear();    
+    // const month = date.getMonth() + 1; // because it starts at 0
+    // const day = date.getDate();
+    // const dateString = `${year}-${month}-${day}`;
 
-    const dateString = `${year}-${month}-${day}`;
+    const dateString = date.toISOString().split('T')[0];
+    const timeString = time.toISOString().split('T')[1];
 
     return new Date(dateString + ' ' + timeString);
 }
