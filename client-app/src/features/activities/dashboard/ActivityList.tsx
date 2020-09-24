eac6faee21c0984ba17e0410c4b043056de0ec3e
pawel <pawel@webperfect.pl>
2020-09-25 01:33:38 +0200

    client-app: update packages

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -16,7 +16,7 @@ const ActivityList = () => {
             {activitiesByDate.map(([group, activities]) => (
                 <Fragment key={group}>
                     <Label size='large' color='blue' >
-                        {format(group, 'eeee do MMMM')}
+                        {format(new Date(group), 'eeee do MMMM')}
                     </Label>
                     <Item.Group divided>
                         {activities.map(activity => (
