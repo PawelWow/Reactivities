13ea58ed25cb676133536e8cc008f17885598971
pawel <pawel@webperfect.pl>
2020-09-24 20:09:54 +0200

    API: Facebook login

--- API/Startup.cs
+++ API/Startup.cs
@@ -152,7 +152,8 @@ namespace API
             services.AddScoped<IPhotoAccessor, PhotoAccessor>();
             services.AddScoped<IProfileReader, ProfileReader>();
             services.Configure<CloudinarySettings>(Configuration.GetSection("Cloudinary"));
-            
+            services.Configure<FacebookAppSettings>(Configuration.GetSection("Authentication:Facebook"));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
